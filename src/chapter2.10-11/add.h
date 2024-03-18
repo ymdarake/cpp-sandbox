969cbfdcdfae4a8e63e60f6a3253ab4d96351a3c
ymdarake <sakamoto.takumi.dev@gmail.com>
2024-03-18 12:24:55 +0900

    :memo: chap 2.10-12

---
+++ src/chapter2.10-11/add.h
@@ -0,0 +1,7 @@
+// #pragma once
+#ifndef ADD_H_CHAP2
+#define ADD_H_CHAP2
+
+int add(int x, int y);
+
+#endif
---
+++ src/chapter2.10-11/main.cpp
@@ -0,0 +1,8 @@
+#include "add.h"
+#include <iostream>
+
+int main(int argc, char const* argv[])
+{
+    std::cout << "sum: " << add(3, 4) << '\n';
+    return 0;
+}
