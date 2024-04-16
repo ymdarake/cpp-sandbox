5508df079e6d89372796593ddd17d8828675064e
ymdarake <sakamoto.takumi.dev@gmail.com>
2024-04-16 16:36:47 +0900

    chapter12.5: optional

---
+++ src/chapter12.15/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <optional> // for std::optional (C++17)
+
+// Our function now optionally returns an int value
+std::optional<int> doIntDivision(int x, int y)
+{
+    if (y == 0)
+        return {}; // or return std::nullopt
+    return x / y;
+}
+
+int main()
+{
+    std::optional<int> result1 { doIntDivision(20, 5) };
+    if (result1)                                       // if the function returned a value
+        std::cout << "Result 1: " << *result1 << '\n'; // get the value
+    else
+        std::cout << "Result 1: failed\n";
+
+    std::optional<int> result2 { doIntDivision(5, 0) };
+
+    if (result2)
+        std::cout << "Result 2: " << *result2 << '\n';
+    else
+        std::cout << "Result 2: failed\n";
+
+    return 0;
+}
